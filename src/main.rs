1ccc0effe6559b1989c32e537252742e4010cc97
Colin Huber <chuber@flyht.com>
2024-04-25 09:16:35 -0600

    env: Initial code.

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let credit_cards = HashMap::from([
+        ("Amy", "1234567 04 25 123"),
+        ("Tim", "1234567 06 27 123"),
+        ("Bob", "1234567 12 27 123"),
+    ]);
+}
